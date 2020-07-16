3a45afd73e6da70c5705967b9545d4975ee6316c
Max Sultan <maxsultakent@gmail.com>
2020-07-15 22:23:28 -0600

    remove menu_items field from restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
   has_many :liked_restaurants
-
+  has_many :menu_items
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_10_213926) do
+ActiveRecord::Schema.define(version: 2020_07_16_042030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 2020_07_10_213926) do
     t.string "name"
     t.string "cuisine"
     t.string "image"
-    t.string "menu_items"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
