63fe69c9ee491d0d9c0ef474da951acc4c4d6992
Max Sultan <maxsultakent@gmail.com>
2020-07-15 22:50:22 -0600

    created a new db table menu_items, gave it a relationship with restaurants, drew the necessary routes, showed on the swipe page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_16_042030) do
+ActiveRecord::Schema.define(version: 2020_07_16_042413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,16 @@ ActiveRecord::Schema.define(version: 2020_07_16_042030) do
     t.index ["restaurant_id"], name: "index_liked_restaurants_on_restaurant_id"
   end
 
+  create_table "menu_items", force: :cascade do |t|
+    t.string "name"
+    t.string "price"
+    t.string "description"
+    t.bigint "restaurant_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["restaurant_id"], name: "index_menu_items_on_restaurant_id"
+  end
+
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "cuisine"
@@ -49,5 +59,6 @@ ActiveRecord::Schema.define(version: 2020_07_16_042030) do
 
   add_foreign_key "liked_restaurants", "groups"
   add_foreign_key "liked_restaurants", "restaurants"
+  add_foreign_key "menu_items", "restaurants"
   add_foreign_key "users", "groups"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,76 +4,84 @@ restaurants = [
                 cuisine: 'sandwhiches', 
                 image: 'https://i.ibb.co/WsJ5ZmG/earnies.png',
                 likedCount: 0, 
-                menuItems: [ {name: 'The Champ', price: '$6.75', description:'sausage egg and cheese on a kaiser roll' }]
+                menuItems: {name: 'The Champ', price: '$6.75', description:'sausage egg and cheese on a kaiser roll' }
             },
             {
                 name: 'Bombay House', 
                 cuisine: 'Indian', 
                 image: 'https://i.ibb.co/wrxZ018/bombay-house.png',
                 likedCount: 0, 
-                menuItems: [ {name: 'Chicken Tiki Masala', price: '$10.75', description:'a classic indian chicken dish with light curry and rice' }]
+                menuItems: {name: 'Chicken Tiki Masala', price: '$10.75', description:'a classic indian chicken dish with light curry and rice' }
             },
             {
                 name: 'Good Thyme', 
                 cuisine: 'health food', 
                 image: 'https://i.ibb.co/4P1kTnC/good-thyme.png',
                 likedCount: 0, 
-                menuItems: [ {name: 'Tri tip', price: '$7.15', description:'a fine cut of steak with argentina chim churri sauce' }]
+                menuItems: {name: 'Tri tip', price: '$7.15', description:'a fine cut of steak with argentina chim churri sauce' }
             },
             {
                 name: 'Maria Bonitas', 
                 cuisine: 'Mexican', 
                 image: 'https://i.ibb.co/8MjFzFC/maria-bonita.png',
                 likedCount: 0, 
-                menuItems: [ {name: 'Flautas', price: '$13.00', description:'cooked chicken or steak wrapped in a tortilla and deep fried' }]
+                menuItems: {name: 'Flautas', price: '$13.00', description:'cooked chicken or steak wrapped in a tortilla and deep fried' }
             },
             {
                 name: 'Five Sushi Bros', 
                 cuisine: 'Sushi', 
                 image: 'https://i.ibb.co/Yph4cYF/five-sushi-bros.png',
                 likedCount: 0, 
-                menuItems: [ {name: 'Angry Bro', price: '$9.00', description:'tempura shrimp and avacado center, layered with yellowtail tuna, topped with tobiko' }]
+                menuItems: {name: 'Angry Bro', price: '$9.00', description:'tempura shrimp and avacado center, layered with yellowtail tuna, topped with tobiko' }
             },
             {
                 name: 'Black Sheep Cafe', 
                 cuisine: 'Modern American', 
                 image: 'https://i.ibb.co/f4W4LXH/black-sheep.png',
                 likedCount: 0, 
-                menuItems: [ {name: 'The Champ', price: '$6.75', description:'sausage egg and cheese on a kaiser roll' }]
+                menuItems: {name: 'The Champ', price: '$6.75', description:'sausage egg and cheese on a kaiser roll' }
             },
             {
                 name: 'Station 22', 
                 cuisine: 'Southern', 
                 image: 'https://i.ibb.co/j3yCqmw/station22.png',
                 likedCount: 0, 
-                menuItems: [ {name: 'Whiskey Chicken', price: '$13.00', description:'Mashed Potatoes, gravy, fried chicken, seasonal vegatables, whiskey sauce and mushrooms' }]
+                menuItems: {name: 'Whiskey Chicken', price: '$13.00', description:'Mashed Potatoes, gravy, fried chicken, seasonal vegatables, whiskey sauce and mushrooms' }
             },
             {
                 name: 'Chillis', 
                 cuisine: 'American', 
                 image: 'https://i.ibb.co/4MnWBFM/chillis.png',
                 likedCount: 0, 
-                menuItems: [ {name: 'Honey Chipotle Chicken Crispers', price: '$11.15', description:'fried chicken in a honey chipotle chicken glaze' }]
+                menuItems: {name: 'Honey Chipotle Chicken Crispers', price: '$11.15', description:'fried chicken in a honey chipotle chicken glaze' }
             },
             {
                 name: 'Two Jacks Pizza', 
                 cuisine: 'Pizza', 
                 image: 'https://i.ibb.co/096nmRf/two-jacks.png',
                 likedCount: 0, 
-                menuItems: [ {name: '14 Inch Cheese Pizza', price: '$14.93', description:'Large pizza dough smother high with our secret morazella blend' }]
+                menuItems: {name: '14 Inch Cheese Pizza', price: '$14.93', description:'Large pizza dough smother high with our secret morazella blend' }
             },
             {
                 name: 'Brick Oven', 
                 cuisine: 'Italian', 
                 image: 'https://i.ibb.co/kxxyF63/brick-oven.png',
                 likedCount: 0, 
-                menuItems: [ {name: 'Chicken Parmesan', price: '$11.22', description:'tender hand-breaded chicken breast topped with melted provolone cheese, served with spaghetti marinara' }]
+                menuItems: {name: 'Chicken Parmesan', price: '$11.22', description:'tender hand-breaded chicken breast topped with melted provolone cheese, served with spaghetti marinara'}
             }
         ]
 
 
 restaurants.each do |r|
-  Restaurant.create(name: r[:name], cuisine: r[:cuisine], image: r[:image], menu_items: r[:menuItems])
+  Restaurant.create(
+      name: r[:name], 
+      cuisine: r[:cuisine], 
+      image: r[:image]
+      ).menu_items.create(
+          name: r[:menuItems][:name], 
+          price: r[:menuItems][:price] , 
+          description: r[:menuItems][:description]
+          )
 end
 
 puts "seeded"
